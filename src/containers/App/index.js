cb2b33daa72262b40f919471f3429dd110e9a42b
Stefan Bibic <stefanbibic55@gmail.com>
2021-08-04 19:54:19 +0200

    import fix

--- src/containers/App/index.js
+++ src/containers/App/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from 'react-query';
 import AllLocation from '../AllLocations';
-import NotFoundPage from '../../components/shared/NotFoundPage';
+import NotFoundPage from '../../components/pages/NotFoundPage';
 
 import './index.scss';
 
