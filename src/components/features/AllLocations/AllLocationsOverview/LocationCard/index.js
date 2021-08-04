7da76f9c39cb197355682eab587e290bed03babf
Stefan Bibic <stefanbibic55@gmail.com>
2021-08-04 19:56:55 +0200

    typo

--- src/components/features/AllLocations/AllLocationsOverview/LocationCard/index.js
+++ src/components/features/AllLocations/AllLocationsOverview/LocationCard/index.js
@@ -16,7 +16,7 @@ import useUserViews from '../../../../../hooks/useUserViews';
 
 const LocationCard = ({ location: { name, userCount, createdAt, id } }) => {
   // It would be better to solve showing of the icon with css and very easy with display:none,
-  // however I had big issues with icons because exports are really bad, they have same id's all over the place and I spent a lot of time just showing them and hover state was easier to do with state
+  // however I had big issues with icons because exports are really bad, they have same id's all over the place and I spent a lot of time just getting them to work and hover state was easier to do with state
   const [isEditIconShown, setIsEditIconShown] = useState(false);
 
   const { views, handleChangeViews } = useUserViews();
